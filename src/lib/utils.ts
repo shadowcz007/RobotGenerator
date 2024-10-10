ee30c63ae2f71c7520c4e3fad664cba23ccf259f
shadowcz007 <chizhiwei007@163.com>
2024-10-10 13:10:11 +0800

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,19 +1,27 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-export function cn(...inputs: ClassValue[]) {
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn (...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function updateJSON(oldJSON:any, newJSON:any) {
+//更新robot的数据
+export function updateRobotJSON (oldJSON: any, newJSON: any) {
   for (let key in oldJSON) {
-      if (newJSON.hasOwnProperty(key)) {
-          if (typeof oldJSON[key] === 'object' && typeof newJSON[key] === 'object') {
-              updateJSON(oldJSON[key], newJSON[key]);
-          } else {
-              oldJSON[key] = newJSON[key];
-          }
+    if (newJSON.hasOwnProperty(key)) {
+      if (
+        typeof oldJSON[key] === 'object' &&
+        typeof newJSON[key] === 'object'
+      ) {
+        updateRobotJSON(oldJSON[key], newJSON[key])
+      } else if (typeof oldJSON[key] === 'string') {
+        //如果新的value，非string
+        oldJSON[key] =
+          typeof newJSON[key] !== 'string'
+            ? JSON.stringify(newJSON[key])
+            : newJSON[key]
       }
+    }
   }
   return oldJSON
 }
