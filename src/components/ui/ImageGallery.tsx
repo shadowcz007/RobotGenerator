9f42923cc605d29e36e74e1d4b2769d171ef6d69
shadowcz007 <chizhiwei007@163.com>
2024-10-10 14:09:10 +0800

    Update ImageGallery.tsx

--- src/components/ui/ImageGallery.tsx
+++ src/components/ui/ImageGallery.tsx
@@ -2,12 +2,12 @@ import { useState, useEffect } from 'react';
 import { Gallery, Item } from 'react-photoswipe-gallery';
 import 'photoswipe/dist/photoswipe.css';
 
-const ImageGallery = ({ images,width,height }: { images: string[],width:number,height:number }) => {
+const ImageGallery = ({ mainImage, moreImages, width, height }: { mainImage: string, moreImages: string[], width: number, height: number }) => {
   const [galleryImages, setGalleryImages] = useState<string[]>([]);
 
   useEffect(() => {
-    setGalleryImages(images);
-  }, [images]);
+    setGalleryImages(moreImages);
+  }, [moreImages]);
 
   const galleryStyle: React.CSSProperties = {
     display: 'flex',
@@ -32,33 +32,59 @@ const ImageGallery = ({ images,width,height }: { images: string[],width:number,h
   };
 
   return (
-    <Gallery>
-      <div style={galleryStyle}>
-        {galleryImages.map((image, index) => (
-          <Item
-            key={index}
-            original={image}
-            thumbnail={image}
+    <div>
+
+      <Gallery>
+        <div className="main-image" style={{ textAlign: 'center', marginBottom: '20px' }}>
+
+          <Item key={0}
+            original={mainImage}
+            thumbnail={mainImage}
             width={width}
-            height={height}
-            // title={`Gallery Image ${index + 1}`} // Add title for better description
-          >
+            height={height}>
+
             {({ ref, open }) => (
               <img
+                src={mainImage}
+                alt="Main"
+                style={{ width: '320px', height: '320px', cursor: 'pointer' }}
                 ref={ref}
                 onClick={open}
-                src={image}
-                alt={`Gallery Image ${index + 1}`}
-                style={thumbnailStyle}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
               />
+
             )}
+
           </Item>
-        ))}
-      </div>
-    </Gallery>
+        </div>
+
+        <div style={galleryStyle}>
+          {galleryImages.map((image, index) => (
+            <Item
+              key={index}
+              original={image}
+              thumbnail={image}
+              width={width}
+              height={height}
+            // title={`Gallery Image ${index + 1}`} // Add title for better description
+            >
+              {({ ref, open }) => (
+                <img
+                  ref={ref}
+                  onClick={open}
+                  src={image}
+                  alt={`Gallery Image ${index + 1}`}
+                  style={thumbnailStyle}
+                  onMouseOver={handleMouseOver}
+                  onMouseOut={handleMouseOut}
+                />
+              )}
+            </Item>
+          ))}
+        </div>
+      </Gallery>
+    </div>
   );
 };
 
 export default ImageGallery;
+
